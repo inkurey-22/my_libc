dcda99fe622dde0cd626a1fc234d99e7d86716d0
Inkurey22 <theophile.riffe@epitech.eu>
2024-10-06 09:48:12 +0200

    feat: added the first 3 functions, very basic

---
+++ my_libc.h
@@ -0,0 +1,19 @@
+/*
+** EPITECH PROJECT, 2024
+** my_libc
+** File description:
+** my_libc header
+*/
+
+#ifndef MY_LIBC_H_
+  #define MY_LIBC_H_
+
+#include <unistd.h>
+#include <limits.h>
+
+int my_strlen(char const *);
+void my_putstr(char const *);
+void my_puterr(char const *);
+
+#endif /* !MY_LIBC_H_ */
+
