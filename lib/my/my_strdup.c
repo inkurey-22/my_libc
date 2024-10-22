0d144a759b57d5775c6a2641345c9ca80b4c5494
Inkurey22 <theophile.riffe@epitech.eu>
2024-10-22 09:37:10 +0200

    fix: modified some path

---
+++ lib/my/my_strdup.c
@@ -0,0 +1,22 @@
+/*
+** EPITECH PROJECT, 2024
+** B-CPE-100-LIL-1-1-cpoolday08-theophile.riffe
+** File description:
+** my_strdup
+*/
+
+#include "my.h"
+
+char *my_strdup(char const *src)
+{
+    int len = my_strlen(src);
+    char *new = malloc(sizeof(char) * (len + 1));
+    int i = 0;
+
+    if (new == NULL)
+        return NULL;
+    for (; src[i] != '\0'; i++)
+        new[i] = src[i];
+    new[i] = '\0';
+    return new;
+}
