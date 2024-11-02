330c561e0eaa7e79756a2be0ca7d4fa401676435
Inkurey22 <inkurey22.tr@proton.me>
2024-11-02 10:51:40 +0100

    reorganization + add of my_printf

--- include/my.h
+++ include/my.h
@@ -6,12 +6,12 @@
 */
 
 #ifndef MY_H_
- #define MY_H_
+    #define MY_H_
 
-    #define BUFF_SIZE 30720
-
+    #include <stdarg.h>
     #include <unistd.h>
     #include <limits.h>
+    #include <stdint.h>
     #include <stdlib.h>
     #include <fcntl.h>
     #include <stdio.h>
@@ -68,5 +68,61 @@ int my_showstr(char const *);
 // Double array Functions
 char **split_string(char const *, char const *);
 int my_show_word_array(char *const *);
+
+/* PRINTF */
+
+typedef struct buffer_s {
+    char buffer[1024];
+    int pos;
+    int len;
+} buffer_t;
+
+typedef struct format_s {
+    int flags;
+    int width;
+    int precision;
+    int length;
+} format_t;
+
+typedef struct printf_s {
+    char c;
+    void (*func)(va_list, buffer_t *, format_t *);
+} printf_t;
+
+void put_buffer(buffer_t *buff, int fd);
+int my_printf(char const *format, ...);
+void manage_specifiers(char c, va_list args, buffer_t *buff, format_t *spec);
+
+void printf_putchar(char c, buffer_t *buff);
+void printf_putstr(char const *str, buffer_t *buff, format_t *flags);
+int printf_putnbr(int n, buffer_t *buff, format_t *flags);
+int my_printf(char const *format, ...);
+char *convert_hex(int long);
+char *convert_oct(int long);
+void printf_putfloat(double, buffer_t *, format_t *);
+char *printf_pointer(void *adress);
+void printf_scientific(double nb, buffer_t *global, format_t *flags, int up);
+int number_len(int n);
+void printf_n_spec(int *n, buffer_t *buff);
+void printf_decimal(int nbr, buffer_t *buff, format_t *flags);
+void printf_g_spec(double nb, buffer_t *buffer, format_t *flags, int up);
+void printf_tab(char **tab, buffer_t *buff);
+
+void print_char(va_list, buffer_t *, format_t *);
+void print_string(va_list, buffer_t *, format_t *);
+void print_int(va_list, buffer_t *, format_t *);
+void print_mudulo(va_list, buffer_t *, format_t *);
+void print_float(va_list, buffer_t *, format_t *);
+void print_pointer(va_list args, buffer_t *buff, format_t *spec);
+void print_puthexmin(va_list args, buffer_t *buff, format_t *spec);
+void print_puthexmaj(va_list args, buffer_t *buff, format_t *spec);
+void print_octal(va_list args, buffer_t *buff, format_t *spec);
+void print_scientific(va_list args, buffer_t *buff, format_t *spec);
+void print_capscientific(va_list args, buffer_t *buff, format_t *spec);
+void print_nspec(va_list args, buffer_t *buff, format_t *spec);
+void parse_format(char const *, int *, format_t *, va_list);
+void print_decimal(va_list args, buffer_t *buff, format_t *spec);
+void print_g_low(va_list args, buffer_t *buff, format_t *spec);
+void print_g_up(va_list args, buffer_t *buff, format_t *spec);
 
 #endif /* !MY_H_ */
